abd5f5d1f369f91575b465566e126ce3039cc4dd
Matthew Taylor <luv2code@gmail.com>
2023-10-20 09:34:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+[Mail-In-A-Box](https://mailinabox.email/) custom DNS API module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with [Mail-In-A-Box](https://mailinabox.email/).
+
+## Caddy Mail-In-A-Box
+
+```
+dns.providers.mailinabox
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "mailinabox",
+				"api_url": "https://[your main-in-a-box domain name]/admin/dns/custom"
+                "email_address": "{$MAIB_EMAIL}"
+                "password": "{$MAIB_PASS}"
+			}
+		}
+	}
+}
+```
+
+or with the Caddyfile:
+
+```
+# globally
+{
+	acme_dns mailinabox {
+        api_url https://[your main-in-a-box domain name]/admin/dns/custom
+        email_address {$MIAB_EMAIL}
+        password {$MIAB_PASS}
+    }
+}
+```
+
+```
+# wild card
+*.[your-root-domain] {
+	tls {
+		dns mailinabox {
+			api_url https://[your box domain name]/admin/dns/custom
+			email_address {$MIAB_EMAIL}
+			password {$MIAB_PASS}
+		}
+	}
+
+	@subdomain host subdomain.[your-root-domain]
+	handle @subdomain {
+        response "Hello on subdomain"
+	}
+}
+```
---
+++ module.go
@@ -0,0 +1,100 @@
+package mailinabox
+
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/libdns/mailinabox"
+)
+
+// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
+type Provider struct{ *mailinabox.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.mailinabox",
+		New: func() caddy.Module { return &Provider{new(mailinabox.Provider)} },
+	}
+}
+
+// Provision sets up the module. Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	p.Provider.APIURL = caddy.NewReplacer().ReplaceAll(p.Provider.APIURL, "")
+	p.Provider.EmailAddress = caddy.NewReplacer().ReplaceAll(p.Provider.EmailAddress, "")
+	p.Provider.Password = caddy.NewReplacer().ReplaceAll(p.Provider.Password, "")
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	mailinabox {
+//	    api_url <api_url>
+//	    email_address <email_address>
+//	    password <password>
+//	}
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_url":
+				if p.Provider.APIURL != "" {
+					return d.Err("API URL already set")
+				}
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				p.Provider.APIURL = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "email_address":
+				if p.Provider.EmailAddress != "" {
+					return d.Err("email address already set")
+				}
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				p.Provider.EmailAddress = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "password":
+				if p.Provider.Password != "" {
+					return d.Err("password already set")
+				}
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				p.Provider.Password = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.APIURL == "" {
+		return d.Err("missing API URL")
+	}
+	if p.Provider.EmailAddress == "" {
+		return d.Err("missing email address")
+	}
+	if p.Provider.Password == "" {
+		return d.Err("missing password")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
